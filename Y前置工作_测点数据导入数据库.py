85f431324f79d55fe3bc6f8e49c03bf4fbdd780c
liuhaojun <haojun.liu@seaboxdata.com>
2025-09-23 09:08:07 +0800

    fix: 测点导入系统统一采用config参数控制

--- Y前置工作_测点数据导入数据库.py
+++ Y前置工作_测点数据导入数据库.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import psycopg2
 from psycopg2 import OperationalError, sql
+from config import AREA, AREA_FILE
+
 
 def create_db_connection():
     """创建数据库连接"""
@@ -75,9 +77,9 @@ def import_to_database(connection, df, table_name, schema_name=None):
 
 if __name__ == "__main__":
     # 配置参数
-    excel_file_path = "/Users/liuhaojun/Documents/项目文档/中国华电项目(云南)/03 时序数据质量稽核规则/09 山东/05 原始数据/山东时序测点04.xlsx"  # 替换为你的Excel文件路径
+    excel_file_path = f'/Users/liuhaojun/Documents/项目文档/中国华电项目(云南)/03 时序数据质量稽核规则/{AREA_FILE}/05 原始数据/{AREA}时序测点04.xlsx'  # 替换为你的Excel文件路径
     target_table = "measure_data"                # 替换为数据库中的目标表名
-    target_schema = "山东区域"                       # 如需要指定模式，替换为模式名，否则为None
+    target_schema = f'{AREA}区域'                        # 如需要指定模式，替换为模式名，否则为None
     
     # 执行流程
     db_conn = create_db_connection()
